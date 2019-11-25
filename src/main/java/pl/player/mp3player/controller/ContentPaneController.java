ccbcdaf3173e8e9e0f48b7ca7e02c07309e2578c
psadovski <pawelsadowski92@gmail.com>
2019-11-25 17:56:27 +0100

    mp3/8 | "Adding lombok and listener to value property"

--- src/main/java/pl/player/mp3player/controller/ContentPaneController.java
+++ src/main/java/pl/player/mp3player/controller/ContentPaneController.java
@@ -2,18 +2,16 @@ package pl.player.mp3player.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Getter
 public class ContentPaneController {
     private static Logger logger = LoggerFactory.getLogger(ContentPaneController.class);
 
     @FXML
     private TableView<?> contentTable;
-
-    public TableView<?> getContentTable() {
-        return this.contentTable;
-    }
 
     public void initialize() {
         logger.info("Content controller created");
--- src/main/java/pl/player/mp3player/controller/MenuPaneController.java
+++ src/main/java/pl/player/mp3player/controller/MenuPaneController.java
@@ -2,9 +2,11 @@ package pl.player.mp3player.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Getter
 public class MenuPaneController {
     private static Logger logger = LoggerFactory.getLogger(MenuPaneController.class);
 
@@ -19,22 +21,6 @@ public class MenuPaneController {
 
     @FXML
     private MenuItem aboutMenuItem;
-
-    public MenuItem getFileMenuItem() {
-        return this.fileMenuItem;
-    }
-
-    public MenuItem getDirMenuItem() {
-        return this.dirMenuItem;
-    }
-
-    public MenuItem getCloseMenuItem() {
-        return this.closeMenuItem;
-    }
-
-    public MenuItem getAboutMenuItem() {
-        return this.aboutMenuItem;
-    }
 
     public void initialize() {
         logger.info("Menu controller created");
